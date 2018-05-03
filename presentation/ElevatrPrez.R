a109ad4e00927300016a794a9b8fe49d0fd3552d
Lauren Bo <lauren.bomeisl@uvm.edu>
2018-05-03 15:10:43 -0400

    finalprezedits

---
+++ presentation/ElevatrPrez.R
@@ -0,0 +1,11 @@
+# Elevatr Prez
+# 2018-05-03
+# LPB
+
+#Prelims
+
+library(elevatr)  # for elevation inqueries
+library(sp)  # for spatial dataset classification
+library(rgdal)  # for transformation
+library(raster)  # for raster plotting
+library(lattice)  # for contour plotting
